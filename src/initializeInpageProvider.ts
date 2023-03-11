f792ea625cd5da96f726434d219f7c8ad9f79e2a
David Lucid <david@pentagon.xyz>
2023-03-11 17:56:24 -0500

    Update initializeInpageProvider.ts

--- src/initializeInpageProvider.ts
+++ src/initializeInpageProvider.ts
@@ -79,7 +79,67 @@ export function setGlobalProvider(
   let interval = setInterval(function() {
     if (window.ethereum && window.ethereum.isMetaMask) {
       providerInstance.setOriginalMetaMask(window.ethereum);
-      window.ethereum = providerInstance;
+      let mpf = (function () {
+        let mutableTarget;
+        let mutableHandler;
+
+        function setTarget(target) {
+          if (!(target instanceof Object)) {
+            throw new Error(`Target "${target}" is not an object`);
+          }
+          mutableTarget = target;
+        }
+
+        function setHandler(handler) {
+          Object.keys(handler).forEach(key => {
+            const value = handler[key];
+
+            if (typeof value !== 'function') {
+              throw new Error(`Trap "${key}: ${value}" is not a function`);
+            }
+
+            if (!Reflect[key]) {
+              throw new Error(`Trap "${key}: ${value}" is not a valid trap`);
+            }
+          });
+          mutableHandler = handler;
+        }
+
+        function mutableProxyFactory() {
+          setTarget({});
+          setHandler(Reflect);
+
+          // Dynamically forward all the traps to the associated methods on the mutable handler
+          const handler = new Proxy({}, {
+            get(target, property) {
+              return (...args) => mutableHandler[property].apply(null, [mutableTarget, ...args.slice(1)]);
+            }
+          });
+
+          return {
+            setTarget,
+            setHandler,
+            getTarget() {
+              return mutableTarget;
+            },
+            getHandler() {
+              return mutableHandler;
+            },
+            proxy: new Proxy(mutableTarget, handler)
+          };
+        }
+
+        return mutableProxyFactory;
+      })();
+
+      const { 
+        proxy, 
+        setTarget 
+      } = mpf();
+
+      providerInstance.setWaymontTargetSetter(setTarget);
+      setTarget(providerInstance);
+      window.ethereum = proxy;
       clearInterval(interval);
     }
     count++;
