b622ca2b5d503eb72c0879b737389f420611627f
David Lucid <david@pentagon.xyz>
2023-03-11 22:37:09 -0500

    Update initializeInpageProvider.ts

--- src/initializeInpageProvider.ts
+++ src/initializeInpageProvider.ts
@@ -82,7 +82,8 @@ export function setGlobalProvider(
       
       /* BEGIN COPIED AND EDITED CODE FROM STACKOVERFLOW: https://stackoverflow.com/a/39481944
          AUTHOR: John Griffing
-         LICENSE: CC BY-SA 3.0: https://creativecommons.org/licenses/by-sa/3.0/legalcode */
+         LICENSE: CC BY-SA 3.0: https://creativecommons.org/licenses/by-sa/3.0/legalcode
+         STACKOVERFLOW LICENSING/COPYRIGHT NOTICE: https://stackoverflow.com/help/licensing */
       let mpf = (function () {
         let mutableTarget;
         let mutableHandler;
