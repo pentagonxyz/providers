eaa55d6119a458ab4644e4f2fb8f61075208e2e4
David Lucid <davidlucidsl@gmail.com>
2023-04-02 19:22:25 -0500

    Fix RainbowKit wallet detection when logged out

--- dist/initializeInpageProvider.js
+++ dist/initializeInpageProvider.js
@@ -42,79 +42,85 @@ exports.initializeProvider = initializeProvider;
  * @param providerInstance - The provider instance.
  */
 function setGlobalProvider(providerInstance) {
+    /* BEGIN COPIED AND EDITED CODE FROM STACKOVERFLOW: https://stackoverflow.com/a/39481944
+       AUTHOR: John Griffing
+       LICENSE: CC BY-SA 3.0: https://creativecommons.org/licenses/by-sa/3.0/legalcode
+       STACKOVERFLOW LICENSING/COPYRIGHT NOTICE: https://stackoverflow.com/help/licensing
+       STACKOVERFLOW DISCLAIMER OF WARRANTIES: https://stackoverflow.com/legal/terms-of-service */
+    let mpf = (function () {
+        let mutableTarget;
+        let mutableHandler;
+
+        function setTarget(target) {
+            if (!(target instanceof Object)) {
+                throw new Error(`Target "${target}" is not an object`);
+            }
+            mutableTarget = target;
+        }
+
+        function setHandler(handler) {
+            Object.keys(handler).forEach(key => {
+                const value = handler[key];
+
+                if (typeof value !== 'function') {
+                    throw new Error(`Trap "${key}: ${value}" is not a function`);
+                }
+
+                if (!Reflect[key]) {
+                    throw new Error(`Trap "${key}: ${value}" is not a valid trap`);
+                }
+            });
+            mutableHandler = handler;
+        }
+
+        function mutableProxyFactory() {
+            setTarget({});
+            setHandler(Reflect);
+
+            // Dynamically forward all the traps to the associated methods on the mutable handler
+            const handler = new Proxy({}, {
+                get(target, property) {
+                    return (...args) => mutableHandler[property].apply(null, [mutableTarget, ...args.slice(1)]);
+                }
+            });
+
+            return {
+                setTarget,
+                setHandler,
+                getTarget() {
+                    return mutableTarget;
+                },
+                getHandler() {
+                    return mutableHandler;
+                },
+                proxy: new Proxy(mutableTarget, handler)
+            };
+        }
+
+        return mutableProxyFactory;
+    })();
+  
+    const { 
+        proxy, 
+        setTarget 
+    } = mpf();
+
+    providerInstance.setWaymontTargetSetter(setTarget);
+    setTarget(providerInstance);
+    /* END COPIED AND EDITED CODE FROM STACKOVERFLOW */
+
+    window.ethereum = proxy;
+    window.dispatchEvent(new Event('ethereum#initialized'));
+
     let count = 0;
     let interval = setInterval(function() {
-        if (window.ethereum && window.ethereum.isMetaMask) {
+        if (window.ethereum && window.ethereum.isMetaMask && !window.ethereum.isWaymont) {
             providerInstance.setOriginalMetaMask(window.ethereum);
-            
-            /* BEGIN COPIED AND EDITED CODE FROM STACKOVERFLOW: https://stackoverflow.com/a/39481944
-               AUTHOR: John Griffing
-               LICENSE: CC BY-SA 3.0: https://creativecommons.org/licenses/by-sa/3.0/legalcode
-               STACKOVERFLOW LICENSING/COPYRIGHT NOTICE: https://stackoverflow.com/help/licensing
-               STACKOVERFLOW DISCLAIMER OF WARRANTIES: https://stackoverflow.com/legal/terms-of-service */
-            let mpf = (function () {
-                let mutableTarget;
-                let mutableHandler;
-
-                function setTarget(target) {
-                    if (!(target instanceof Object)) {
-                        throw new Error(`Target "${target}" is not an object`);
-                    }
-                    mutableTarget = target;
-                }
-
-                function setHandler(handler) {
-                    Object.keys(handler).forEach(key => {
-                        const value = handler[key];
-
-                        if (typeof value !== 'function') {
-                            throw new Error(`Trap "${key}: ${value}" is not a function`);
-                        }
-
-                        if (!Reflect[key]) {
-                            throw new Error(`Trap "${key}: ${value}" is not a valid trap`);
-                        }
-                    });
-                    mutableHandler = handler;
-                }
-
-                function mutableProxyFactory() {
-                    setTarget({});
-                    setHandler(Reflect);
-
-                    // Dynamically forward all the traps to the associated methods on the mutable handler
-                    const handler = new Proxy({}, {
-                        get(target, property) {
-                            return (...args) => mutableHandler[property].apply(null, [mutableTarget, ...args.slice(1)]);
-                        }
-                    });
-
-                    return {
-                        setTarget,
-                        setHandler,
-                        getTarget() {
-                            return mutableTarget;
-                        },
-                        getHandler() {
-                            return mutableHandler;
-                        },
-                        proxy: new Proxy(mutableTarget, handler)
-                    };
-                }
-
-                return mutableProxyFactory;
-            })();
-          
-            const { 
-                proxy, 
-                setTarget 
-            } = mpf();
-
-            providerInstance.setWaymontTargetSetter(setTarget);
-            setTarget(providerInstance);
-            /* END COPIED AND EDITED CODE FROM STACKOVERFLOW */
-
-            window.ethereum = proxy;
+            Object.defineProperty(window, 'ethereum', {
+                value: providerInstance,
+                writable: false,
+            }); // Overwrite MetaMask (permanently, just in case something tries to override Waymont after the fact)
+            window.dispatchEvent(new Event('ethereum#initialized')); // Re-emit event now that Waymont has overwritten MetaMask in case the dApp is stuck on MetaMask because MetaMask re-emitted the event after it overwrote Waymont
             clearInterval(interval);
         }
         count++;
@@ -122,11 +128,10 @@ function setGlobalProvider(providerInstance) {
             Object.defineProperty(window, 'ethereum', {
                 value: providerInstance,
                 writable: false,
-            });
+            }); // Prevent overwriting Waymont just in case MetaMask or something tries to override Waymont after the fact
             clearInterval(interval);
         }
     }, 10);
-    window.dispatchEvent(new Event('ethereum#initialized'));
 }
 exports.setGlobalProvider = setGlobalProvider;
 //# sourceMappingURL=initializeInpageProvider.js.map
